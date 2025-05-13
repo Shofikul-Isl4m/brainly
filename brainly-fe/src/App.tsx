b6021c6dab5b634441d1df2f09d011e26781334a
Shofikul islam <shofikul.islam23102023@gmail.com>
2025-05-13 11:28:59 +0600

    add button

---
+++ brainly-fe/src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/Button";
+import { PlusIcon } from "./icons/PlusIcon";
+import { ShareIcon } from "./icons/ShareIcon";
+
+function App() {
+  return (
+    <div>
+      <Button variant="primary" text="Add content" startIcon={<PlusIcon />} />
+      <Button
+        variant="secondary"
+        text="Share Brain"
+        startIcon={<ShareIcon />}
+      />
+    </div>
+  );
+}
+
+export default App;
