fd2d71cf1d15004d8303fbf74ad817d255a16b32
Shofikul islam <shofikul.islam23102023@gmail.com>
2025-05-14 11:49:14 +0600

    add createcontentmodal component

---
+++ brainly-fe/src/components/CreateContentModal.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { CrossIcon } from "../icons/CrossIcon";
+import { Button } from "./Button";
+
+export function CreateContentModal({ open, onClose }) {
+  return (
+    <div>
+      {open && (
+        <div className="w-screen h-screen bg-slate-200 fixed top-0 left-0 opacity-60 flex justify-center ">
+          <div className="flex flex-col justify-center">
+            <span className="bg-white opacity-100 p-4 rounded">
+              <div className="flex justify-end ">
+                <div onClick={onClose}>
+                  <CrossIcon />
+                </div>
+              </div>
+              <div>
+                <Input placeholder={"title"} />
+                <Input placeholder={"Link"} />
+              </div>
+              <div className="flex justify-center">
+                <Button variant="primary" text="Submit" />
+              </div>
+            </span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Input({ onchange, placeholder }: { onChange: () => void }) {
+  return (
+    <div>
+      <input
+        type={"text"}
+        placeholder={placeholder}
+        className="px-4 py-2 m-2 border rounded m-2"
+        onChange={onchange}
+      ></input>
+    </div>
+  );
+}
